b98d5538bda2bd517fed77ee9d1fe46c2c41f149
wangff <1024904798@qq.com>
2019-08-29 16:51:56 +0800

    算法总结

--- src/main/java/com/wangff/learning/api/config/AppClientConfig.java
+++ src/main/java/com/wangff/learning/api/config/AppClientConfig.java
@@ -2,21 +2,20 @@ package com.wangff.learning.api.config;
 
 import com.nettyrpc.client.RpcClient;
 import com.nettyrpc.client.proxy.RpcClientProxyBeanPostProcessor;
-import com.nettyrpc.registry.ServiceDiscovery;
 import org.springframework.context.annotation.*;
 
 @Configuration
 public class AppClientConfig {
 
-    @Bean(name = "serviceDiscovery")
-    public ServiceDiscovery serviceDiscovery () {
-        return new ServiceDiscovery("127.0.0.1:2181");
-    }
+//    @Bean(name = "serviceDiscovery")
+//    public ServiceDiscovery serviceDiscovery () {
+//        return new ServiceDiscovery("127.0.0.1:2181");
+//    }
 
     @Bean(name = "rpcClient")
-    @DependsOn("serviceDiscovery")
-    public RpcClient rpcClient (ServiceDiscovery serviceDiscovery) {
-        return new RpcClient(serviceDiscovery);
+    public RpcClient rpcClient () {
+//        return new RpcClient("list://127.0.0.1:18866");
+        return new RpcClient("zookeeper://127.0.0.1:2181");
     }
 
     @Bean
